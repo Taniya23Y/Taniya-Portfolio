155c7b90ad4479ccc52a592b9f50112876f94f3c
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-04-11 22:28:59 +0530

    Finish: lib section

--- components/projects.tsx
+++ components/projects.tsx
@@ -6,12 +6,12 @@ import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
 
-export default function projects() {
+export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
-  
+
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
-      <SectionHeading>My Projects</SectionHeading>
+      <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
